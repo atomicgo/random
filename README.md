2b63e31f61e72560ab0b864e252db0b928fb8d94
MarvinJWendt <git@marvinjwendt.com>
2023-08-29 21:49:01 +0000

    docs: autoupdate

--- README.md
+++ README.md
@@ -69,7 +69,9 @@
 import "atomicgo.dev/random"
 ```
 
-Package random provides random generators.
+Package random provides random generators for most inbuilt types \(int, float, string, bool, etc...\).
+
+All functions are easily accessible and don't require any setup.
 
 ## Index
 
--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package random provides random generators for most inbuilt types (int, float, string, bool, etc...). 
+Package random provides random generators for most inbuilt types (int, float, string, bool, etc...).
 
 All functions are easily accessible and don't require any setup.
 */
