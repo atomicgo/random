75ba902a19f305e71bab4d8a241f203d2c8ffd42
Marvin Wendt <git@marvinjwendt.com>
2023-08-29 23:48:05 +0200

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 /*
-Package random provides random generators.
+Package random provides random generators for most inbuilt types (int, float, string, bool, etc...). 
+
+All functions are easily accessible and don't require any setup.
 */
 package random
